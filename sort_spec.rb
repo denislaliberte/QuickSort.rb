e132d356bcbf652e962fbfc479e86a88eaef9a10
Denis Laliberté <denis@tp1.ca>
2015-02-08 18:50:23 -0500

    sort return an empty list

--- sort_spec.rb
+++ sort_spec.rb
@@ -1,7 +1,10 @@
-
+require "./sort.rb"
 
 describe "sort" do
   it "pass the canary test" do
     expect(true).to eq(true)
+  end
+  it "return an empty list" do
+    expect(sort([])).to eq([])
   end
 end
