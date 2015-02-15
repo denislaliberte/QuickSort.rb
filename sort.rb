1540bb8b504fe294a6446bc116388a5b97b62e8e
Denis Laliberté <denis@tp1.ca>
2015-02-15 14:31:53 -0500

    sort muttiple pivot

--- sort.rb
+++ sort.rb
@@ -3,7 +3,10 @@ def sort(input)
   if input.size <=1
     input
   else
-    smaller = input.keep_if{ |element| element < 2}
-    smaller.push(2)
+    pivot = input.first
+    bigger = input.select{ |a| a > pivot}
+    pivots = input.select{ |a | a == pivot}
+    smaller = input.select{ |a| a < pivot}
+    return smaller.concat(pivots).concat(bigger)
   end
 end
--- sort_spec.rb
+++ sort_spec.rb
@@ -10,4 +10,10 @@ describe "sort" do
   it "sort smaller elements" do
     expect(sort([2,1,1,1])).to eq([1,1,1,2])
   end
+  it "sort bigger elements" do 
+    expect(sort([2,1,3])).to eq([1,2,3])
+  end
+  it "sort with mutliple pivot" do 
+    expect(sort([20,20,10,30])).to eq([10,20,20,30])
+  end
 end
