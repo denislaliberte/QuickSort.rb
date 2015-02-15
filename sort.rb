9bc003182f6bba3e453b78dade5b1dda28f53e88
Denis Laliberté <denis@tp1.ca>
2015-02-15 14:13:04 -0500

    sort smaller elements

--- sort.rb
+++ sort.rb
@@ -1,4 +1,9 @@
 
 def sort(input)
-  input
+  if input.size <=1
+    input
+  else
+    smaller = input.keep_if{ |element| element < 2}
+    smaller.push(2)
+  end
 end
--- sort_spec.rb
+++ sort_spec.rb
@@ -4,4 +4,10 @@ describe "sort" do
   it "return an empty list" do
     expect(sort([])).to eq([])
   end
+  it "sort two unsorted element" do
+    expect(sort([2,1])).to eq([1,2])
+  end
+  it "sort smaller elements" do
+    expect(sort([2,1,1,1])).to eq([1,1,1,2])
+  end
 end
